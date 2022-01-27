77cd832d4256d9d486b4779c20d5349560e02421
andrew zimatskiy <zimazky@gmail.com>
2022-01-27 19:51:19 +0300

    Некоторые изменения

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,14 +1,15 @@
 import styles from './CalendarDay.module.css'
 import DateTime from '../utils/datetime.js'
 
-export default function CalendarDay({timestamp, dayHeight, actualBalance, plannedBalance, plannedBalanceChange, onAddEvent=()=>{}, children = null}) {
+export default function CalendarDay({data, onAddEvent=()=>{}, children = null}) {
+  const {timestamp, actualBalance, plannedBalance, plannedBalanceChange} = data
   const inputElementRef = React.useRef(null)
   const {day, month} = DateTime.getDayMonthWeekday(timestamp)
 
   function onClickHandle(e) {
     if(inputElementRef) {
       //console.log(inputElementRef.current)
-      inputElementRef.current.focus()
+      //inputElementRef.current.focus()
     }
   }
   function onKeyDownHandle(e) {
@@ -24,17 +25,18 @@ export default function CalendarDay({timestamp, dayHeight, actualBalance, planne
   const plus = d => d>0?'+'+d.toFixed(1):d.toFixed(1)
 
   return (
-    <div className={styles.CalendarDay} style={{height: dayHeight}} onClick={onClickHandle}>
+    <td className={styles.CalendarDay} onClick={onClickHandle}>
       <div className={styles.CalendarDayHeader}>{day + (day==1?' '+DateTime.MONTHS[month]:'') }</div>
       <div className={styles.balance}>{minimize(plannedBalance) + 
         (plannedBalanceChange==0?'k':plus(plannedBalanceChange/1000)+'k') +
         ' ' + minimize(actualBalance)}</div>
       <div className="CalendarDayTasks"> {children} </div>
+      {/*
       <div className={styles.DayTaskInput}>
         <input ref={inputElementRef} rows={1} wrap='off' onBlur={onBlurHandle} onKeyDown={onKeyDownHandle}></input>
       </div>
+      */}
 
-
-    </div> 
+    </td> 
     )
 }
--- src/components/CalendarWeek.jsx
+++ src/components/CalendarWeek.jsx
@@ -1,35 +1,18 @@
 import EventItem, {EventPlaceholder} from "./EventItem.jsx"
 import styles from './CalendarWeek.module.css'
+import CalendarDay from "./CalendarDay.jsx"
 
 const min = (a,b)=>a<b?a:b
-const minimize = d => (d/1000).toFixed(1)
-const plus = d => d>0?'+'+d.toFixed(1):d.toFixed(1)
 
 export default function CalendarWeek({week, onEventClick=(event)=>{}}) {
-  const inputElementRef = React.useRef(null)
 
   return (
-    <>
-      <tr>{week.map( d=><td key={d.timestamp}>
-        <div className={styles.day}>{new Date(d.timestamp*1000).getDate()}</div>
-        <div className={styles.balance}>{
-          minimize(d.plannedBalance) + 
-          (d.plannedBalanceChange==0?'k':plus(d.plannedBalanceChange/1000)+'k') +
-          ' ' + minimize(d.actualBalance)}</div></td>)}</tr>
-      <tr className={styles.events}>{
-        week.map( (d,j) => (
-          <td key={d.timestamp}>{ 
-              d.tasks.map((t,i)=>{
-              if(t.id === -1) return <EventPlaceholder key={i}/>
-              return <EventItem key={i} event={t} days={min(t.days,7-j)}
-              onClick={onEventClick}/>
-            })}
-          <div className={styles.DayTaskInput}>
-            <input ref={inputElementRef} rows={1} wrap='off' /*onBlur={onBlurHandle} onKeyDown={onKeyDownHandle}*/></input>
-          </div>
-
-          </td>
-        ))}</tr>
-    </>
+    <tr className={styles.week}>{week.map( (d,j)=><CalendarDay key={d.timestamp} data={d}> {
+      d.tasks.map((t,i)=>{
+        if(t.id === -1) return <EventPlaceholder key={i}/>
+        return <EventItem key={i} event={t} days={min(t.days,7-j)} onClick={onEventClick}/>
+      })}
+      </CalendarDay>
+      )}</tr>
   )
 }
