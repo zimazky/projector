9f5153d768f91681f37bcaca249bec7e68465ee2
Andrey Zimatskiy <zimazky@gmail.com>
2022-02-15 15:05:57 +0300

    Исправлена работа при отсутствии сохраненной версии в localStorage

--- src/model/data.js
+++ src/model/data.js
@@ -1,6 +1,6 @@
 import EventList from '../utils/eventList'
 
-const json = localStorage.getItem('data')
+const json = localStorage.getItem('data') ?? '{}'
 console.log('localStorage',json)
 const obj = JSON.parse(json)
 export const eventList = new EventList(obj)
