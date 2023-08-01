88e1838f34f0e0d1bd33d590fdb1a425a352015f
Andrey Zimatskiy <zimazky@gmail.com>
2023-08-01 18:33:52 +0300

    Исправлено отображение дней в многодневных событиях

--- src/components/Calendar/CalendarEventItem.tsx
+++ src/components/Calendar/CalendarEventItem.tsx
@@ -11,8 +11,8 @@ type EventItemProps = {
 }
 
 export default function EventItem(props: EventItemProps) {
-  const {timestamp, days} = props
-  const {id, name, completed, background, color, repeatable, start, time, end, credit, debit} = props.event
+  const {timestamp} = props
+  const {id, name, completed, background, color, repeatable, start, time, end, credit, debit, days} = props.event
 
   const openEventForm = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation()
@@ -33,7 +33,7 @@ export default function EventItem(props: EventItemProps) {
   <div className={completed?styles.completed:repeatable?styles.repeatable:styles.item}
     draggable={true} onDragStart={onDragStart}
     style={{
-      width: days==1?'calc(100% + 2px)':'calc(' +days +' * (100% + 1px) + 1px )',
+      width: props.days==1?'calc(100% + 2px)':'calc(' +props.days +' * (100% + 1px) + 1px )',
       backgroundColor: background,
       color: color
     }} 
@@ -48,7 +48,7 @@ export default function EventItem(props: EventItemProps) {
         timestamp-start === 0 ? name : `${(timestamp-start)/86400+1}/${(end-start)/86400} ${name}`
         }</div> <div className={styles.time}>{
         time === null ?
-        (days-days>0?` ${(timestamp-start)/86400+days}/${(end-start)/86400}`:'')
+        (days-props.days>0?` ${(timestamp-start)/86400+props.days}/${(end-start)/86400}`:'')
         : DateTime.secondsToHMM(time)
       }</div>
   </div>)
